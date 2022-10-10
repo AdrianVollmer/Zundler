9a4f8207a63f5fbaf18f9317e4a91bd86009bda9
Adrian Vollmer <computerfluesterer@protonmail.com>
2022-10-10 16:00:23 +0200

    Add test case

---
+++ tests/demo001/conf.py
@@ -0,0 +1,140 @@
+from sphinx.application import Sphinx
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'demo001'
+copyright = '2022, Adrian Vollmer'
+author = 'Adrian Vollmer'
+version = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+master_doc = "index"
+language = "en"
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "myst_parser",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.viewcode",
+    "sphinx_design",
+]
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "sphinx_book_theme"
+html_logo = "_static/logo-wide.svg"
+html_favicon = "_static/logo-square.svg"
+html_title = ""
+html_theme_options = {
+    "home_page_in_toc": True,
+    "github_url": "https://github.com/AdrianVollmer/Zundler",
+    "repository_url": "https://github.com/AdrianVollmer/Zundler",
+    "repository_branch": "master",
+    "path_to_docs": "docs",
+    "use_repository_button": True,
+    "use_edit_page_button": True,
+}
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["_static"]
+
+myst_enable_extensions = [
+    "dollarmath",
+    "amsmath",
+    "deflist",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+    "colon_fence",
+    "smartquotes",
+    "replacements",
+    "linkify",
+    "strikethrough",
+    "substitution",
+    "tasklist",
+    #  "attrs_image",
+]
+myst_number_code_blocks = ["typescript"]
+myst_heading_anchors = 2
+myst_footnote_transition = True
+myst_dmath_double_inline = True
+
+suppress_warnings = ["myst.strikethrough"]
+
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3.7", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master", None),
+    "markdown_it": ("https://markdown-it-py.readthedocs.io/en/latest", None),
+}
+
+# autodoc_default_options = {
+#     "show-inheritance": True,
+#     "special-members": "__init__, __enter__, __exit__",
+#     "members": True,
+#     # 'exclude-members': '',
+#     "undoc-members": True,
+#     # 'inherited-members': True
+# }
+autodoc_member_order = "bysource"
+nitpicky = True
+nitpick_ignore = [
+    ("py:class", "docutils.nodes.document"),
+    ("py:class", "docutils.nodes.docinfo"),
+    ("py:class", "docutils.nodes.Element"),
+    ("py:class", "docutils.nodes.Node"),
+    ("py:class", "docutils.nodes.field_list"),
+    ("py:class", "docutils.nodes.problematic"),
+    ("py:class", "docutils.nodes.pending"),
+    ("py:class", "docutils.nodes.system_message"),
+    ("py:class", "docutils.statemachine.StringList"),
+    ("py:class", "docutils.parsers.rst.directives.misc.Include"),
+    ("py:class", "docutils.parsers.rst.Parser"),
+    ("py:class", "docutils.utils.Reporter"),
+    ("py:class", "nodes.Element"),
+    ("py:class", "nodes.Node"),
+    ("py:class", "nodes.system_message"),
+    ("py:class", "Directive"),
+    ("py:class", "Include"),
+    ("py:class", "StringList"),
+    ("py:class", "DocutilsRenderer"),
+    ("py:class", "MockStateMachine"),
+]
+
+
+def setup(app: Sphinx):
+    """Add functions to the Sphinx setup."""
+    from myst_parser._docs import (
+        DirectiveDoc,
+        DocutilsCliHelpDirective,
+        MystConfigDirective,
+    )
+
+    app.add_css_file("custom.css")
+    app.add_directive("myst-config", MystConfigDirective)
+    app.add_directive("docutils-cli-help", DocutilsCliHelpDirective)
+    app.add_directive("doc-directive", DirectiveDoc)
---
+++ tests/demo001/index.md
@@ -0,0 +1,25 @@
+Welcome to demo001's documentation!
+===================================
+
+```{toctree}
+:maxdepth: 2
+:caption: 'Contents:'
+
+first
+second
+third
+paragraph-markup
+```
+
+```{image} _static/logo-square.svg
+:width: 200px
+:class: sd-m-auto
+```
+
+
+Chapters
+========
+
+* {ref}`first`
+* {ref}`second`
+* {ref}`third`
