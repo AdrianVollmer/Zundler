351582e055ebcb93b1be0ef8a75b3e7d51297cbd
Adrian Vollmer <computerfluesterer@protonmail.com>
2022-10-10 17:41:41 +0200

    Add more source pragmas

--- zundler/assets/inject.js
+++ zundler/assets/inject.js
@@ -23,7 +23,7 @@ var embed_js = function() {
                 var src = newScript.getAttribute('src');
                 let [path, get_parameters, anchor] = split_url(src);
                 path = normalize_path(path);
-                var src = retrieve_file(path);
+                var src = retrieve_file(path) + ' //# ' + path;
                 newScript.appendChild(document.createTextNode(src));
                 newScript.removeAttribute('src');
                 oldScript.parentNode.replaceChild(newScript, oldScript);
