c58206b892be6ad94fa095ec7ed899e692b93af7
Adrian Vollmer <computerfluesterer@protonmail.com>
2024-03-31 10:54:35 +0200

    Add black check to nox file

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,14 @@
 import nox
 
+
 @nox.session()
 def tests(session):
     session.install(".[tests]")
-    session.run("pytest", '--driver=firefox', *session.posargs)
+    session.run("pytest", "--driver=firefox", *session.posargs)
+
+
+@nox.session()
+def black(session):
+    session.install(".[tests]")
+    session.run("black", "zundler", "--check", *session.posargs)
+    session.run("black", "tests", "--check", *session.posargs)
